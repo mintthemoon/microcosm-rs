067a14f51af6da33139a7e244e62fcc20154dab0
mintthemoon <105956535+mintthemoon@users.noreply.github.com>
2024-06-27 04:54:46 +0000

    0.1.3 metaquery

--- src/msg.rs
+++ src/msg.rs
@@ -1,6 +1,6 @@
 use crate::{
     schema::cw_serde,
-    page::PageMsg,
+    page::{PageMsg, PageQuery},
 };
 
 #[cw_serde]
@@ -12,4 +12,15 @@ pub struct MetadataMsg {
 pub struct MetaMsg<T> {
     meta: MetadataMsg,
     data: T,
+}
+
+#[cw_serde]
+pub struct MetadataQuery {
+    pub page: Option<PageQuery>,
+}
+
+#[cw_serde]
+pub struct MetaQuery<T> {
+    meta: Option<MetadataQuery>,
+    query: T,
 }
